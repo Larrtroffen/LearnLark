ae055b4c597e10cd75fc3a5db38064343f5ed436
Larrtroffen <Larrtroffen@outlook.com>
2024-03-06 21:30:38 +0800

    update interface

--- client/src/renderer/src/App.vue
+++ client/src/renderer/src/App.vue
@@ -22,5 +22,13 @@
 </template>
 
 <style>
+.common-layout {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+
 
 </style>
--- client/src/renderer/src/layout/Header.vue
+++ client/src/renderer/src/layout/Header.vue
@@ -3,14 +3,27 @@
 </script>
 
 <template>
-    <div>
-      <h1>LearnLark — 吴林昊团队十年精研</h1>
-    </div>
+    <el-header class="topbar --el-large-radius-base" >
+      <h1 class="title">LearnLark — 吴林昊团队十年巨作</h1>
+    </el-header>
   </template>
 
-<style>
-  h1 {
-    text-align: start;
+<style scoped>
+  .title {
+    text-align: center;
     margin-top: 10px;
+  }
+  .topbar {
+    border-bottom: 1px solid var(--border-color);
+    background-color: #0047c1ab;
+    color: white;
+    position: sticky;
+    top: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    padding: 0 32px;
+    justify-content: space-between;
+
   }
 </style>
