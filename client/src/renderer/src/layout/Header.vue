39d2256fad18d1137ca6222fd78e2551f5b511ca
Larrtroffen <Larrtroffen@outlook.com>
2024-03-08 12:49:10 +0800

    update Header+icon

--- client/src/renderer/src/layout/Header.vue
+++ client/src/renderer/src/layout/Header.vue
@@ -3,24 +3,31 @@
 </script>
 
 <template>
-  <el-header>
     <div class="top-bar">
-      <el-row>
-        <el-col :span="4">
-          <el-button >学习</el-button>
-          <el-button round>测试</el-button>
+          <el-button class="link-btn">
+            <div>学习</div>
+          </el-button>
+          <div class="sep1"></div>
+          <el-button class="link-btn">
+            <div>测试</div>
+          </el-button>
+          <div class="sep"></div>
           <img class="logo" src="..\pics\logo1.png" alt="Logo">
-          <el-button round>记录</el-button>
-          <el-button round>我的</el-button>
-        </el-col>
-      </el-row>
+          <div class="sep"></div>
+          <el-button class="link-btn">
+            <div>记录</div>
+          </el-button>
+          <div class="sep1"></div>
+          <el-button class="link-btn">
+            <div>我的</div>
+          </el-button>
     </div>
-  </el-header>
   </template>
 
 <style scoped>
   .top-bar {
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
   background-color: rgb(255, 255, 255);
@@ -30,8 +37,19 @@
   width: 100px; 
   height: auto;
 }
-.el-row {
-  margin: 10px 0;
+.sep {
+  width: 20px;
 }
-
+.sep1 {
+  width: 40px;
+}
+.link-btn {
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-bottom: 2px solid #72b0ed;
+  font-size: 18px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
 </style>
